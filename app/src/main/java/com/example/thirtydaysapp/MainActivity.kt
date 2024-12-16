d716a12c3766d627a54eebce5fa369d08c1fecff
Evans Ogidigo <ogidigoevans@gmail.com>
2024-12-16 07:42:10 +0100

    final commit

--- app/src/main/java/com/example/thirtydaysapp/MainActivity.kt
+++ app/src/main/java/com/example/thirtydaysapp/MainActivity.kt
@@ -6,17 +6,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -29,9 +36,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.thirtydaysapp.data.DayTips
 import com.example.thirtydaysapp.data.dayTips
 import com.example.thirtydaysapp.ui.theme.ThirtyDaysAppTheme
 
@@ -59,10 +68,12 @@ fun ThirtyDaysApp() {
         topBar =  {
             AppTopBar()
         }
-    ) {
-        LazyColumn {
+    ) {it ->
+        LazyColumn(
+            contentPadding = it
+        ) {
             items(dayTips){
-
+                DayTipCard(streak = it.streak, tasksHeading = it.tasks, imageRes = it.imageTask, details = it.descriptionRes)
             }
         }
     }
@@ -83,7 +94,12 @@ fun AppTopBar(){
 }
 
 @Composable
-fun DayTipCard(){
+fun DayTipCard(
+    streak: Int,
+    tasksHeading: Int,
+    imageRes: Int,
+    details: Int
+){
     var showMore by remember {
         mutableStateOf(false)
     }
@@ -99,20 +115,31 @@ fun DayTipCard(){
             Column (
                 Modifier.padding(16.dp)
             ){
-                Text(
-                    text = "Day 1",
-                    style = MaterialTheme.typography.displayLarge
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ){
+                    Text(
+                        text = "Day$streak",
+                        style = MaterialTheme.typography.displayLarge
                     )
+                    if (showMore){
+                        Icon(Icons.Filled.KeyboardArrowUp, null)
+                    } else {
+                        Icon(Icons.Filled.KeyboardArrowDown, null)
+                    }
+                }
+
                 Text(
-                    text = "Spend 35 minutes outdoors",
+                    text = stringResource(id = tasksHeading),
                     fontSize = 24.sp
                     )
                 Box {
-                    Image(painter = painterResource(id = R.drawable.deadpool_and_wolverine_panic_at_every_turn_p3), contentDescription = null )
+                    Image(painter = painterResource(id = imageRes), contentDescription = null )
                 }
                 if (showMore){
                     Text(
-                        text = "Try to code every day",
+                        text = stringResource(id = details),
                         style = MaterialTheme.typography.displaySmall
                         )
                 }
@@ -127,6 +154,6 @@ fun DayTipCard(){
 @Composable
 fun GreetingPreview() {
     ThirtyDaysAppTheme {
-        DayTipCard()
+        ThirtyDaysApp()
     }
 }
--- app/src/main/java/com/example/thirtydaysapp/data/DayTips.kt
+++ app/src/main/java/com/example/thirtydaysapp/data/DayTips.kt
@@ -2,9 +2,10 @@ package com.example.thirtydaysapp.data
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import com.example.thirtydaysapp.R
 
 data class DayTips(
-    @StringRes val streak: Int,
+     val streak: Int,
     @StringRes val tasks: Int,
     @DrawableRes val imageTask: Int,
     @StringRes val descriptionRes: Int
@@ -12,7 +13,183 @@ data class DayTips(
 
 val dayTips = listOf(
     DayTips(
-        streak = ,
-        tasks =
+        streak = 1,
+        tasks = R.string.task_breathing,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_breathing
+    ),
+    DayTips(
+        streak = 2,
+        tasks = R.string.task_morning_walk,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_morning_walk
+    ),
+    DayTips(
+        streak = 3,
+        tasks = R.string.task_gratitude_journal,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_journal
+    ),
+    DayTips(
+        streak = 4,
+        tasks = R.string.task_healthy_snack,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_snack
+    ),
+    DayTips(
+        streak = 5,
+        tasks = R.string.task_meditation,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_meditation
+    ),
+    DayTips(
+        streak = 6,
+        tasks = R.string.task_stretching,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_stretching
+    ),
+    DayTips(
+        streak = 7,
+        tasks = R.string.task_social_connection,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_social_connection
+    ),
+    DayTips(
+        streak = 8,
+        tasks = R.string.task_drink_water,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_drink_water
+    ),
+    DayTips(
+        streak = 9,
+        tasks = R.string.task_nature_walk,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_nature_walk
+    ),
+    DayTips(
+        streak = 10,
+        tasks = R.string.task_read_book,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_read_book
+    ),
+    DayTips(
+        streak = 11,
+        tasks = R.string.task_positive_affirmations,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_positive_affirmations
+    ),
+    DayTips(
+        streak = 12,
+        tasks = R.string.task_exercise,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_exercise
+    ),
+    DayTips(
+        streak = 13,
+        tasks = R.string.task_music_relaxation,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_music_relaxation
+    ),
+    DayTips(
+        streak = 14,
+        tasks = R.string.task_plan_goals,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_plan_goals
+    ),
+    DayTips(
+        streak = 15,
+        tasks = R.string.task_self_reflection,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_self_reflection
+    ),
+    DayTips(
+        streak = 16,
+        tasks = R.string.task_healthy_meal,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_healthy_meal
+    ),
+    DayTips(
+        streak = 17,
+        tasks = R.string.task_call_friend,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_call_friend
+    ),
+    DayTips(
+        streak = 18,
+        tasks = R.string.task_random_act_kindness,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_random_act_kindness
+    ),
+    DayTips(
+        streak = 19,
+        tasks = R.string.task_mindful_eating,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_mindful_eating
+    ),
+    DayTips(
+        streak = 20,
+        tasks = R.string.task_journal_reflection,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_journal_reflection
+    ),
+    DayTips(
+        streak = 21,
+        tasks = R.string.task_spend_time_outdoors,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_spend_time_outdoors
+    ),
+    DayTips(
+        streak = 22,
+        tasks = R.string.task_unplug_tech,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_unplug_tech
+    ),
+    DayTips(
+        streak = 23,
+        tasks = R.string.task_try_new_hobby,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_try_new_hobby
+    ),
+    DayTips(
+        streak = 24,
+        tasks = R.string.task_declutter_space,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_declutter_space
+    ),
+    DayTips(
+        streak = 25,
+        tasks = R.string.task_laugh_more,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_laugh_more
+    ),
+    DayTips(
+        streak = 26,
+        tasks = R.string.task_sleep_well,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_sleep_well
+    ),
+    DayTips(
+        streak = 27,
+        tasks = R.string.task_listen_podcast,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_listen_podcast
+    ),
+    DayTips(
+        streak = 28,
+        tasks = R.string.task_relax_bath,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_relax_bath
+    ),
+    DayTips(
+        streak = 29,
+        tasks = R.string.task_practice_gratitude,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_practice_gratitude
+    ),
+    DayTips(
+        streak = 30,
+        tasks = R.string.task_review_progress,
+        imageTask = R.drawable.thirtydayimage,
+        descriptionRes = R.string.desc_review_progress
     )
 )
